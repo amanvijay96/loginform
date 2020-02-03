d47286a964d247d6869150114f249a068df763ea
aman_vj <aman.vijay@greytip.com>
2020-02-03 18:56:41 +0530

    [WIP] Frontend

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './home.css'
+
+class Home extends React.Component {
+render() {
+  return (
+   <div className="home" >
+      <textarea className="query-div" type="text" placeholder="Write your query" />
+      <hr />
+        Hi
+      </div>
+  )
+}
+}
+
+export default Home;
